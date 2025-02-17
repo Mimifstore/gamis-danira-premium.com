391d9b731a5f81cd9736e7f473caa16f212faf30
Mimifstore <168345866+Mimifstore@users.noreply.github.com>
2025-02-17 12:19:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -107,6 +107,17 @@ let index = 0;
             window.open(whatsappURL, '_blank');
         }
 
+(function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)})(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '601816316053339'); 
+fbq('track', 'PageView');
 
 
        
